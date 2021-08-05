1de5f3e86065d9f332ece4f8c6574f730856167a
yoona610 <xiaotengxinai@gmail.com>
2021-08-05 05:06:46 +0000

    [ADD]ユーザー新規登録・ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #deviseのルーティング
+  devise_for :users,
+    controllers: {
+      sessions: 'public/devise/sessions',
+      passwords: 'public/devise/passwords',
+      registrations: 'public/devise/registrations',
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
