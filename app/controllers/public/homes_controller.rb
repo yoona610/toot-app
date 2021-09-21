51d6652ff3dcc9c21bc37b8b5446dbed2d4c54c6
yoona610 <xiaotengxinai@gmail.com>
2021-09-21 05:02:32 +0000

    [UPDATE]homes/controller修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.includes(:user).where(is_draft: false).order(created_at: 'DESC').limit(6)
+    @posts = Post.includes(:user).where(is_draft: false).order(created_at: 'DESC').limit(8)
     @post_ranks = Post.create_post_ranks.first(6)
   end
 
