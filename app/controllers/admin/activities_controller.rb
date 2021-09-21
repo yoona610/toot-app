bf50102d7eaa17aecf1b29bc85ba8dd94eadf937
yoona610 <xiaotengxinai@gmail.com>
2021-09-21 11:42:22 +0000

    [UPDATE]通知機能のキャッシュ削除

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ActivitiesController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_cache_buster, only: %i[index]
 
   def index
     @activities = Activity.includes(subject: :user).where(activity_status: 'admin_activity').order(created_at: 'DESC').page(params[:page]).per(20)
@@ -10,5 +11,13 @@ class Admin::ActivitiesController < ApplicationController
     activity = Activity.includes(:subject).find(params[:id])
     activity.read! if activity.unread?
     redirect_to activity.redirect_path
+  end
+
+  private
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
--- app/controllers/public/activities_controller.rb
+++ app/controllers/public/activities_controller.rb
@@ -1,5 +1,6 @@
 class Public::ActivitiesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_cache_buster, only: %i[index]
 
   def index
     @activities = Activity.includes(:subject).where(user_id: current_user.id,
@@ -12,5 +13,13 @@ class Public::ActivitiesController < ApplicationController
     activity = Activity.includes(:subject).find(params[:id])
     activity.read! if activity.unread?
     redirect_to activity.transition_path
+  end
+
+  private
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
